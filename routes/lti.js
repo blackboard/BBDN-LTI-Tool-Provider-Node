eaf85706556bab38e849981ea24b8e29a08fedaa
EC2 Default User <ec2-user@ip-10-202-159-238.ec2.internal>
2016-01-12 12:03:33 -0500

    first commit

---
+++ routes/lti.js
@@ -0,0 +1,73 @@
+var lti = require('ims-lti');
+var _ = require('lodash');
+
+//for testing
+var consumer_key = "12345";
+var consumer_secret = "secret";
+var sourcedid = "bbgc46gi65";
+var lisoutcomesendpoint="https://cdev-saas-original-prod.blackboard.com/webapps/gradebook/lti11grade";
+
+
+
+
+/*
+ * POST LTI Launch Received
+ */
+
+exports.got_launch = function(req, res){
+
+  var provider = new lti.Provider(consumer_key, consumer_secret);
+  req.body = _.omit(req.body, '__proto__');
+
+  console.log(req.headers);
+  console.log(req.body);
+  
+  var content = "";
+  
+  var keys = Object.keys( req.body );
+  for( var i = 0,length = keys.length; i < length; i++ ) {
+     content += keys[i] + " = " + req.body[ keys[ i ] ] + "<br />";
+  }
+  
+  provider.valid_request(req, function(err, isValid) {
+     if(err) {
+         console.log(err);
+	 res.send(403);
+     }
+     else {
+ 	 if (!isValid) res.send(422);
+
+	 res.render('lti', { title: 'LTI Launch Received!', content: content });
+     }
+  });
+};
+
+exports.caliper = function(req, res) {
+
+};
+
+exports.outcomes = function(req,res) {
+
+  var provider = new lti.Provider(consumer_key, consumer_secret);
+  req.body = _.omit(req.body, '__proto__');
+
+  provider.valid_request(req, function(err, isValid) {
+     if(err) {
+         console.log(err);
+         res.send(403);
+     }
+     else {
+       if (!isValid || !provider.outcome_service) return false;
+
+       //Check if the outcome service supports the result data extension using the
+       //text format. Available formats include text and url.
+       console.log(provider.outcome_service.supports_result_data('text'));
+
+       //Replace accepts a value between 0 and 1.
+       provider.outcome_service.send_replace_result(.5, function(err,result) {
+         console.log(result); //True or false
+       });
+     }
+  });
+
+};
