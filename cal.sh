d26d98e9c9ad4acbb2770ce736a8c3cd12d9bb8f
Scott Hurrey <Scott.Hurrey@blackboard.com>
2016-09-21 16:53:51 -0400

    Cleanup repository

--- cal.sh
+++
@@ -1,5 +0,0 @@
-npm uninstall ims-caliper
-cd /Users/shurrey/Documents/workspace/ims/ims-caliper
-npm pack
-cd /Users/shurrey/Documents/workspace/ims/BBDN-LTI-Tool-Provider-Node
-npm install /Users/shurrey/Documents/workspace/ims/ims-caliper/ims-caliper-1.0.0.tgz
